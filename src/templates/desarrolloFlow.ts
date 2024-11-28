24d393ae04c20ce5a17496bd024cb40c409564e8
smasetto <smasetto@mzsolutionsbi.com>
2024-11-28 08:47:49 -0300

    add idle

--- src/templates/desarrolloFlow.ts
+++ src/templates/desarrolloFlow.ts
@@ -1,15 +1,24 @@
 import { addKeyword,EVENTS } from "@builderbot/bot"
+import { reset, start,stop } from "utils/idle-custom"
 
 
 
 
 const desarrolloFlow = addKeyword(EVENTS.ACTION)
-                    .addAnswer('🙌 ¡Genial! Podrías indicarnos tu nombre completo',{capture:true})
+                    .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow, 10000))
+                    .addAnswer('🙌 ¡Genial! Podrías indicarnos tu nombre completo',{capture:true},
+                        async (ctx, { gotoFlow, state }) => {
+                            reset(ctx, gotoFlow, 10000);
+                            })
                     .addAnswer('¿Algún horario preferencia para comunicarnos?',{
                         capture:true
-                    })
+                    },async (ctx, { gotoFlow, state }) => {
+                        reset(ctx, gotoFlow, 10000);
+                        })
                     .addAnswer('🙌 ¡Excelente! Un agente se contactará a la brevedad. Escribe *menu* si quieres volver al menú principal.',{
                         capture:true
+                    },async (ctx, { gotoFlow, state }) => {
+                        stop(ctx);
                     })
 
 export{desarrolloFlow}
