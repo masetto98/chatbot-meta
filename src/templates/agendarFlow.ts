8678cbcc1328f1173dad7506153ddb2add38ac28
smasetto <smasetto@mzsolutionsbi.com>
2024-12-03 10:33:36 -0300

    fix

--- src/templates/agendarFlow.ts
+++ src/templates/agendarFlow.ts
@@ -24,6 +24,27 @@ function formatDateForMySQL(dateString: string): string {
   
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
+// Función para formatear la fecha en palabras
+function formatDateInWords(date) {
+    const days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+    const months = [
+        'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+        'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+    ];
+    
+    const dayName = days[date.getDay()];
+    const dayNumber = date.getDate();
+    const monthName = months[date.getMonth()];
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedTime = `${hours % 12 || 12}:${minutes} ${ampm}`;
+    return `${dayName} ${dayNumber} de ${monthName} ${year} a las ${formattedTime}`;
+}
+
+
 
 const afirmative3 = addKeyword('Sí')
                     .addAction(async (ctx,ctxFn) => {
@@ -218,7 +239,9 @@ const visitaFlow = addKeyword(EVENTS.ACTION)
             else{
                 const nextAvailableslot = await getNextAvailableSlot(startDate)
                 await ctxFn.state.update({nextAvailableslot:nextAvailableslot})
-                await ctxFn.flowDynamic(`😅 ¡No tengo disponibilidad para la fecha solicitada! Te parece si lo agendamos para el día: ${nextAvailableslot.start.toLocaleString()}`)
+               
+                await ctxFn.flowDynamic(`😅 ¡No tengo disponibilidad para la fecha solicitada! Te parece si lo agendamos para el día: ${formatDateInWords(nextAvailableslot.start)}`)
+                //await ctxFn.flowDynamic(`😅 ¡No tengo disponibilidad para la fecha solicitada! Te parece si lo agendamos para el día: ${nextAvailableslot.start.toLocaleString()}`)
                 return await ctxFn.gotoFlow(noavailableFlow)
             }
 
