728e381c304c527a379fba78a85d61462cac5fbd
smasetto <smasetto@mzsolutionsbi.com>
2024-12-03 11:22:22 -0300

    add tasacionFlow y desarrolloFlow

--- src/templates/desarrolloFlow.ts
+++ src/templates/desarrolloFlow.ts
@@ -1,17 +1,60 @@
 import { addKeyword,EVENTS } from "@builderbot/bot"
-import { reset, start,stop } from "utils/idle-custom"
+import { createEvent } from "~/scripts/calendar"
+import { welcomeFlow } from "./welcomeFlow";
 
 
 
+const afirmativeFlow = addKeyword('Sí')
+                        .addAnswer('🙌 Antes de agendar la reunión, queremos conocer algunos detalles...')
+                        .addAnswer('¿Qué tipo de propiedad estás buscando?',{
+                            capture:true,
+                            delay:2000,
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({tipoPropiedad:ctx.body})
+                        })
+                        .addAnswer('¿Tenés algun presupuesto en mente?',{
+                            capture:true
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({presupuesto:ctx.body})
+                        })
+                        .addAnswer('Por último, indicame tu nombre y apellido',{
+                            capture:true
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({cliente:ctx.body})
+                        })
+                        .addAction(async (ctx,ctxFn) => {
+                            const eventName = "Desarrollo"
+                            const name = await ctxFn.state.get('cliente')
+                            const tipoProp = await ctxFn.state.get('tipoPropiedad')
+                            const presp = await ctxFn.state.get('presupuesto')
+                            const description = `Nombre: ${name}, tel: ${ctx.from}, asunto: le interesa ${tipoProp} y tiene un presupuesto de ${presp}` 
+                            const date = new Date()
+                            date.setHours(date.getHours() + 1)
+                            const eventId = await createEvent(eventName,description,date.toISOString(),0.1)
+                            await ctxFn.state.update({intention:undefined})
+                            ctxFn.flowDynamic(`¡Genial! 🤗 Un agente se contactará a la brevedad. Ante cualquier otra consulta no dudes en escribirme.`)
+                        })
+                        
+                        
 
+
+const negativeFlow = addKeyword('No').addAction(async(ctx,ctxFn) => {
+    return ctxFn.gotoFlow(welcomeFlow)
+})
+
+    
+                
 const desarrolloFlow = addKeyword(EVENTS.ACTION)
-                    .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow, 10000))
-                    .addAnswer('🙌 ¡Genial! Podrías indicarnos tu nombre completo',{capture:true})
-                    .addAnswer('¿Algún horario preferencia para comunicarnos?',{
-                        capture:true
-                    })
-                    .addAnswer('🙌 ¡Excelente! Un agente se contactará a la brevedad. Ante cualquier otra consulta no dudes en escribirme.',{
-                        capture:true
-                    })
-
+                        .addAnswer('😄 ¡Genial! Estamos para ayudarte a encontrar la propiedad de tus sueños. 🤗 ¿Coordinamos una reunión para conocer más en detalle tus necesidades?'
+                            ,{
+                            capture:true,
+                            buttons: [
+                                {body:'Sí'},
+                                {body:'No'},
+                            ]
+                        },null,[afirmativeFlow,negativeFlow])
+    
 export{desarrolloFlow}
--- src/templates/tasacionFlow.ts
+++ src/templates/tasacionFlow.ts
@@ -1,15 +1,60 @@
 import { addKeyword,EVENTS } from "@builderbot/bot"
+import { createEvent } from "~/scripts/calendar"
+import { welcomeFlow } from "./welcomeFlow";
 
 
 
+const afirmativeFlow = addKeyword('Sí')
+                        .addAnswer('🙌 Antes de agendar la reunión, queremos conocer algunos detalles...')
+                        .addAnswer('¿Qué tipo de propiedad queres tasar?',{
+                            capture:true,
+                            delay:2000,
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({tipoPropiedad:ctx.body})
+                        })
+                        .addAnswer('¿Dónde está ubicada?',{
+                            capture:true
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({ubic:ctx.body})
+                        })
+                        .addAnswer('Por último, indicame tu nombre y apellido',{
+                            capture:true
+                        },
+                        async (ctx,ctxFn) => {
+                            await ctxFn.state.update({cliente:ctx.body})
+                        })
+                        .addAction(async (ctx,ctxFn) => {
+                            const eventName = "Tasación"
+                            const name = await ctxFn.state.get('cliente')
+                            const tipoProp = await ctxFn.state.get('tipoPropiedad')
+                            const ubic = await ctxFn.state.get('ubic')
+                            const description = `Nombre: ${name}, tel: ${ctx.from}, asunto: le interesa ${tipoProp} y tiene un presupuesto de ${ubic}` 
+                            const date = new Date()
+                            date.setHours(date.getHours() + 1)
+                            const eventId = await createEvent(eventName,description,date.toISOString(),0.1)
+                            await ctxFn.state.update({intention:undefined})
+                            ctxFn.flowDynamic(`¡Genial! 🤗 Un agente se contactará a la brevedad. Ante cualquier otra consulta no dudes en escribirme.`)
+                        })
+                        
+                        
 
+
+const negativeFlow = addKeyword('No').addAction(async(ctx,ctxFn) => {
+    return ctxFn.gotoFlow(welcomeFlow)
+})
+
+    
+                
 const tasacionFlow = addKeyword(EVENTS.ACTION)
-                    .addAnswer('🙌 ¡Genial! Podrías indicarnos tu nombre completo',{capture:true})
-                    .addAnswer('¿Algún horario preferencia para comunicarnos?',{
-                        capture:true
-                    },)
-                    .addAnswer('🙌 ¡Excelente! Un agente se contactará a la brevedad. Escribe *menu* si quieres volver al menú principal.',{
-                        capture:true
-                    },)
-
+                        .addAnswer('😄 ¡Genial! Estamos para ayudarte a encontrar la propiedad de tus sueños. 🤗 ¿Coordinamos una reunión para conocer más en detalle tus necesidades?'
+                            ,{
+                            capture:true,
+                            buttons: [
+                                {body:'Sí'},
+                                {body:'No'},
+                            ]
+                        },null,[afirmativeFlow,negativeFlow])
+    
 export{tasacionFlow}
