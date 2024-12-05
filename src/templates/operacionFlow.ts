58d59e22029b797854ac110fe9c9b1ec04c0f517
smasetto <smasetto@mzsolutionsbi.com>
2024-12-05 16:13:47 -0300

    asd

--- src/templates/operacionFlow.ts
+++ src/templates/operacionFlow.ts
@@ -55,7 +55,7 @@ const operacionFlow = addKeyword(EVENTS.ACTION)
             chattest = modelo.startChat({
             generationConfig: {
                 maxOutputTokens: 300,  // Adjust based on desired response length
-                temperature:0.2,
+                temperature:0.1,
                 /*topP:0.8,
                 topK:20*/
             },
